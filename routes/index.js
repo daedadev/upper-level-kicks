f56bcd9c488dd19368236e8c738a9fe0403b9b3a
Daniel Moore <wron2@hotmail.com>
2022-01-23 12:42:51 -0800

    Setting up basic express server

---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const router = require("express").Router();
+
+router.get("/api");
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/index");
+const sequelize = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
