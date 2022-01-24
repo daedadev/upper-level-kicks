903beee0e906e882a084bf0df1f63dcf72f8e418
Daniel Moore <wron2@hotmail.com>
2022-01-24 14:41:22 -0800

    Added testing routes for basic information return

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./apiRoutes");
 const loginRoutes = require("./loginRoutes");
+const testRoutes = require("./testRoutes");
 
 router.use("/api", apiRoutes);
 router.use("/log", loginRoutes);
+router.use("/test", testRoutes);
 
 module.exports = router;
