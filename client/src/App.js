913472e5dcf273901a54d830a8ccba73edd6e327
Daniel Moore <wron2@hotmail.com>
2022-01-25 21:50:50 -0800

    Styled footer and contact form components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import MainPage from "./pages/MainPage";
 import Header from "./components/header";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
