1027bf48ba4c1fbc901824f450318c127f4f63ef
Daniel Moore <wron2@hotmail.com>
2022-02-05 02:51:10 -0800

    Updated some styling and updated specific call that would break the project

--- client/src/components/ResultPageItem/index.js
+++ client/src/components/ResultPageItem/index.js
@@ -7,7 +7,7 @@ export default function ResultPageItem({ shoe, shoeFunction }) {
     // Held inside of .result-large-item-holder
     <Link
       to={{
-        pathname: `/result/${shoe.shoeName}?style=${shoe.styleID}&make=${shoe.silhoutte}`,
+        pathname: `/result/${shoe.brand}?style=${shoe.styleID}&make=${shoe.silhoutte}`,
       }}
     >
       <div className="result-item-holder">
