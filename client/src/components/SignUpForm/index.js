a2f3b397c947be8a8093b1212ef8858b45223bb5
Daniel Moore <wron2@hotmail.com>
2022-02-08 00:33:32 -0800

    Added dual signup login form

--- client/src/components/SignUpForm/index.js
+++ client/src/components/SignUpForm/index.js
@@ -13,50 +13,83 @@ export default function SignUpForm() {
 
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  const [formType, setFormType] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
 
     if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+      console.log("bruh");
       return setError("Passwords do not match");
+    }
+
+    if (passwordRef.current.value == "" || emailRef.current.value == "") {
+      return setError("Please enter an email and password");
     }
     try {
       setError("");
       setLoading(true);
       signup(emailRef.current.value, passwordRef.current.value);
-      navigate("/");
     } catch (err) {
       setError("Failed to create an account");
     }
+    navigate("/");
     setLoading(false);
   }
 
+  function changeFormType(type) {
+    setFormType(type);
+  }
+
+  const loginForm = (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <div>Log In</div>
+      {error && <label className="danger-label">{error}</label>}
+      <label>Email</label>
+      <input type="text" className="login-input" ref={emailRef}></input>
+      <label>Password</label>
+      <input type="text" className="login-input" ref={passwordRef}></input>
+      <button type="submit" className="login-button" disabled={loading}>
+        Log In
+      </button>
+      <button onClick={googlePopup} className="google-button">
+        <img src="https://cdn.cdnlogo.com/logos/g/35/google-icon.svg" />
+        Sign In With Google
+      </button>
+    </form>
+  );
+
+  const signupForm = (
+    <form className="login-form" onSubmit={handleSubmit}>
+      <div>Sign Up</div>
+      {error && <label className="danger-label">{error}</label>}
+      <label>Email</label>
+      <input type="text" className="login-input" ref={emailRef}></input>
+      <label>Password</label>
+      <input type="text" className="login-input" ref={passwordRef}></input>
+      <label>Confirm Password</label>
+      <input
+        type="text"
+        className="login-input"
+        ref={confirmPasswordRef}
+      ></input>
+      <button type="submit" className="login-button" disabled={loading}>
+        Sign Up
+      </button>
+
+      <button className="google-button" onClick={googlePopup}>
+        <img src="https://cdn.cdnlogo.com/logos/g/35/google-icon.svg" />
+        Google Sign In
+      </button>
+    </form>
+  );
+
   return (
     <div className="login-form-holder">
-      <form className="login-form" onSubmit={handleSubmit}>
-        <div>Sign Up</div>
-        {error && <label className="danger-label">{error}</label>}
-        <label>Email</label>
-        <input type="text" className="login-input" ref={emailRef}></input>
-        <label>Password</label>
-        <input type="text" className="login-input" ref={passwordRef}></input>
-        <label>Confirm Password</label>
-        <input
-          type="text"
-          className="login-input"
-          ref={confirmPasswordRef}
-        ></input>
-        <button type="submit" className="login-button" disabled={loading}>
-          Sign Up
-        </button>
-
-        <button className="google-button" onClick={googlePopup}>
-          <img src="https://cdn.cdnlogo.com/logos/g/35/google-icon.svg" />
-          Google Sign In
-        </button>
-      </form>
+      {formType ? loginForm : signupForm}
       <div>
-        Already have an account? <Link to="/login">Log In Here</Link>
+        <button onClick={() => changeFormType(false)}>Sign Up</button>
+        <button onClick={() => changeFormType(true)}>Log in</button>
       </div>
     </div>
   );
